89a4c72118ed981b6615d140be0405038afe29d6
raviMukti <shinjiravi@gmail.com>
2021-11-04 17:37:18 +0700

    Inisialisasi

---
+++ src/main/java/spring/micro/oms/SpringMicroOmsApplication.java
@@ -0,0 +1,13 @@
+package spring.micro.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroOmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroOmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/micro/oms/SpringMicroOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.micro.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroOmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
