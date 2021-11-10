130e655861618cbab7016575365feb4a407882f9
raviMukti <shinjiravi@gmail.com>
2021-11-10 19:44:39 +0700

    Menambahkan Migrasi Data Category

---
+++ src/main/resources/db/migration/V2.0.0.1__Data_Category.sql
@@ -0,0 +1,5 @@
+insert into category ("name", active, created_date, updated_date, created_by, updated_by) values ('PAKAIAN PRIA', true, NOW(), NOW(), 0, 0);
+insert into category ("name", active, created_date, updated_date, created_by, updated_by) values ('PAKAIAN WANITA', true, NOW(), NOW(), 0, 0);
+insert into category ("name", active, created_date, updated_date, created_by, updated_by) values ('MAKANAN', true, NOW(), NOW(), 0, 0);
+insert into category ("name", active, created_date, updated_date, created_by, updated_by) values ('MINUMAN', true, NOW(), NOW(), 0, 0);
+insert into category ("name", active, created_date, updated_date, created_by, updated_by) values ('ELEKTRONIK', true, NOW(), NOW(), 0, 0);
