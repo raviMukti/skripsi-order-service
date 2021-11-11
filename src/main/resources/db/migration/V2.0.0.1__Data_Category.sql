1910a5a5e5e248a65e744a056ee9f085ef44eded
raviMukti <shinjiravi@gmail.com>
2021-11-11 11:59:48 +0700

    Adding category_id to be inserted manually

--- src/main/resources/db/migration/V2.0.0.1__Data_Category.sql
+++ src/main/resources/db/migration/V2.0.0.1__Data_Category.sql
@@ -1,5 +1,5 @@
-insert into category ("name", active, created_date, updated_date, created_by, updated_by) values ('PAKAIAN PRIA', true, NOW(), NOW(), 0, 0);
-insert into category ("name", active, created_date, updated_date, created_by, updated_by) values ('PAKAIAN WANITA', true, NOW(), NOW(), 0, 0);
-insert into category ("name", active, created_date, updated_date, created_by, updated_by) values ('MAKANAN', true, NOW(), NOW(), 0, 0);
-insert into category ("name", active, created_date, updated_date, created_by, updated_by) values ('MINUMAN', true, NOW(), NOW(), 0, 0);
-insert into category ("name", active, created_date, updated_date, created_by, updated_by) values ('ELEKTRONIK', true, NOW(), NOW(), 0, 0);
+insert into category (category_id, "name", active, created_date, updated_date, created_by, updated_by) values (1, 'PAKAIAN PRIA', true, NOW(), NOW(), 0, 0);
+insert into category (category_id, "name", active, created_date, updated_date, created_by, updated_by) values (2, 'PAKAIAN WANITA', true, NOW(), NOW(), 0, 0);
+insert into category (category_id, "name", active, created_date, updated_date, created_by, updated_by) values (3, 'MAKANAN', true, NOW(), NOW(), 0, 0);
+insert into category (category_id, "name", active, created_date, updated_date, created_by, updated_by) values (4, 'MINUMAN', true, NOW(), NOW(), 0, 0);
+insert into category (category_id, "name", active, created_date, updated_date, created_by, updated_by) values (5, 'ELEKTRONIK', true, NOW(), NOW(), 0, 0);
